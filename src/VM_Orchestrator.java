b603dd0fbdef3519bf7575d4df6363c168df4486
samesser <35739540+samesser@users.noreply.github.com>
2020-04-10 14:30:28 -0500

    Initial orchestrator
    May want to rewrite our listener to a more API model, but this is functional.

--- src/VM_Orchestrator.java
+++ src/VM_Orchestrator.java
@@ -1,9 +1,102 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Base64;
+import java.util.Scanner;
 
 public class VM_Orchestrator {
-
-	public static void main(String[] args) {
-		//stub for main
-
+	
+	//System variables
+	static int port = 9991; //default port, changes with config file
+	static String SQLip;
+	static String SQLuser;
+	static String SQLpass;
+	static String welcomeMessage = "Connected! Enter 'exit' to shutdown orchestrator";
+	static String closedMessage = "Connection Closed...";
+	
+    public static void main(String[] args) {
+        if(getConfig()) {
+        	startServer();
+        }
+    }
+    	
+    private static boolean getConfig() {
+    	try {
+    	      File config = new File("src/config.cfg");
+    	      Scanner configReader = new Scanner(config);
+    	      while (configReader.hasNextLine()) {
+    	        String setting = configReader.nextLine();
+    	        if(setting != "") {
+    	        	setVariable(setting);
+    	        }
+    	      }
+    	      configReader.close();
+    	      return true;
+    	    } catch (FileNotFoundException e) {
+    	      System.out.println("Error: No config file!");
+    	      return false;
+    	    }
 	}
 
+	private static void setVariable(String setting) {
+		setting.replaceAll(" ", "");
+		String[] configArray = setting.split("=");
+		//Set Port
+		if(configArray[0].compareTo("port") == 0) {
+			port = Integer.parseInt(configArray[1]);
+		}
+		//Set SQL ip address
+		else if(configArray[0].compareTo("SQLip") == 0) {
+			SQLip = configArray[1];
+		}
+		//Set SQL user name and password decoded from base64 user:pass
+		else if(configArray[0].compareTo("SQLlogin") == 0) {
+			byte[] decodedBytes = Base64.getDecoder().decode(configArray[1]);
+			String decodedString = new String(decodedBytes);
+			String[] userArray = decodedString.split(":");
+			SQLuser = userArray[0];
+			SQLpass = userArray[1];
+		}
+	}
+
+	public static void startServer() {
+        try(ServerSocket serverSocket = new ServerSocket(port)) {
+            Socket connectionSocket = serverSocket.accept();
+
+            //Create Input&Outputstreams for the connection
+            InputStream inputToServer = connectionSocket.getInputStream();
+            OutputStream outputFromServer = connectionSocket.getOutputStream();
+
+            Scanner scanner = new Scanner(inputToServer, "UTF-8");
+            PrintWriter serverPrintOut = new PrintWriter(new OutputStreamWriter(outputFromServer, "UTF-8"), true);
+            
+            //Welcome message
+            serverPrintOut.println(welcomeMessage);
+
+            boolean exit = false;
+
+            while(!exit && scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                
+                parseCommand(serverPrintOut, line);
+
+                if(line.toLowerCase().trim().equals("exit")) {
+                    exit = true;
+                    serverPrintOut.println(closedMessage);
+                    serverPrintOut.close();
+                    scanner.close();
+                    System.exit(0);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	
+	public static boolean parseCommand(PrintWriter output, String input) {
+		//TODO: Write command parser. All command data will come in as input string, the current code works
+		//input string should be in json format but the code doesn't currently enforce it for testing with telnet
+		output.println("Command: " + input + " recieved");
+		return true;
+	}
 }
